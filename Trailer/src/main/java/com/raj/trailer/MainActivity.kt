49d5c35bd68b8b5b6bc8bd739907f0d708a061c7
Raj Pratap Singh Jadon <rajjadon@gofynd.com>
2023-07-21 03:05:08 +0530

    add dagger hilt at project level

--- Trailer/src/main/java/com/raj/trailer/MainActivity.kt
+++ Trailer/src/main/java/com/raj/trailer/MainActivity.kt
@@ -16,7 +16,9 @@ import com.raj.trailer.navigation.AppBottomNavigation
 import com.raj.trailer.navigation.BottomNavigationScreens
 import com.raj.trailer.navigation.bottomNavigationItems
 import com.raj.presentation.ui.theme.TrailerTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
