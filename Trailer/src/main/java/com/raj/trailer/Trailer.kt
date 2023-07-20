78a545cc4bcd5727d6a42105b8ff2a0941520140
Raj Pratap Singh Jadon <rajjadon@gofynd.com>
2023-07-21 02:49:46 +0530

    add dagger hilt at project level

--- Trailer/src/main/java/com/raj/trailer/Trailer.kt
+++ Trailer/src/main/java/com/raj/trailer/Trailer.kt
@@ -1,13 +1,15 @@
 package com.raj.trailer
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class Trailer : Application() {
     override fun onCreate() {
         super.onCreate()
 
-//        if (BuildConfig.DEBUG)
-//            Timber.plant(Timber.DebugTree())
+        /*if (BuildConfig.DEBUG)
+            Timber.plant(Timber.DebugTree())*/
     }
 }
