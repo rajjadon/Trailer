825c988c89f2a36164f7c316db7567334494b9bb
Raj Pratap Singh Jadon <rajjadon@gofynd.com>
2023-07-22 19:00:22 +0530

    add loading and error screen

--- presentation/src/main/java/com/raj/presentation/ui/component/UiComponent.kt
+++ presentation/src/main/java/com/raj/presentation/ui/component/UiComponent.kt
@@ -3,13 +3,11 @@ package com.raj.presentation.ui.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
@@ -33,56 +31,53 @@ import com.raj.common.error.NetworkError
 @Composable
 @Preview
 fun HomeScreenItem() {
-    Box(
+    Column(
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.Center,
         modifier = Modifier
             .padding(5.dp)
             .background(color = Color.Gray, shape = RoundedCornerShape(10.dp))
     ) {
-        Column(
-            horizontalAlignment = Alignment.End,
-            verticalArrangement = Arrangement.Center
-        ) {
 
-            /*GlideImage(
-                model = "", contentDescription = "movie banner", modifier = Modifier
-                    .height(160.dp)
-                    .clip(shape = RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp))
-            )*/
+        /*GlideImage(
+            model = "", contentDescription = "movie banner", modifier = Modifier
+                .height(160.dp)
+                .clip(shape = RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp))
+        )*/
 
-            Image(
-                painter = ColorPainter(Color.White),
-                contentDescription = "movie image",
-                modifier = Modifier
-                    .height(160.dp)
-                    .clip(shape = RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp))
+        Image(
+            painter = ColorPainter(Color.White),
+            contentDescription = "movie image",
+            modifier = Modifier
+                .height(160.dp)
+                .clip(shape = RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp))
+        )
+        Text(
+            text = "Title",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    top = 16.dp, start = 10.dp
+                ),
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
             )
-            Text(
-                text = "Title",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        top = 16.dp, start = 10.dp
-                    ),
-                style = TextStyle(
-                    color = Color.White,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold
-                )
+        )
+        Text(
+            text = "Description",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    start = 10.dp, bottom = 16.dp
+                ),
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal
             )
-            Text(
-                text = "Description",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        start = 10.dp, bottom = 16.dp
-                    ),
-                style = TextStyle(
-                    color = Color.White,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Normal
-                )
-            )
-        }
+        )
     }
 }
 
@@ -93,78 +88,72 @@ class SampleNetworkErrorProvider : PreviewParameterProvider<NetworkError> {
 @Composable
 @Preview
 fun ErrorUi(@PreviewParameter(SampleNetworkErrorProvider::class) networkError: NetworkError) {
-    Box {
-        Column(
-            horizontalAlignment = Alignment.End,
-            verticalArrangement = Arrangement.Center,
+    Column(
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Text(
+            text = networkError.getErrorCode().toString(),
+            textAlign = TextAlign.Center,
             modifier = Modifier
                 .fillMaxWidth()
-                .fillMaxHeight()
-        ) {
-            Text(
-                text = networkError.getErrorCode().toString(),
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        top = 16.dp, start = 10.dp
-                    ),
-                style = TextStyle(
-                    color = Color.White,
-                    fontSize = 35.sp,
-                    fontWeight = FontWeight.Bold
-                )
+                .padding(
+                    top = 16.dp, start = 10.dp
+                ),
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 35.sp,
+                fontWeight = FontWeight.Bold
             )
-            Text(
-                text = networkError.getNetworkErrorMessage(),
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        start = 10.dp, bottom = 16.dp
-                    ),
-                style = TextStyle(
-                    color = Color.White,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Normal
-                )
+        )
+        Text(
+            text = networkError.getNetworkErrorMessage(),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    start = 10.dp, bottom = 16.dp
+                ),
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal
             )
-        }
+        )
     }
 }
 
 @Composable
 @Preview
 fun LoadingUi(loadingMessage: String = "Loading data please wait") {
-    Box {
-        Column(
-            horizontalAlignment = Alignment.End,
-            verticalArrangement = Arrangement.Center,
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .background(color = Color.Gray)
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        CircularProgressIndicator(
+            color = Color.White
+        )
+
+        Text(
+            text = loadingMessage,
+            textAlign = TextAlign.Center,
             modifier = Modifier
                 .fillMaxWidth()
-                .fillMaxHeight()
-        ) {
-            CircularProgressIndicator(
-                color = Color.Gray,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 6.dp)
-                    .size(36.dp)
+                .padding(
+                    start = 10.dp, bottom = 16.dp
+                ),
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal
             )
-            Text(
-                text = loadingMessage,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        start = 10.dp, bottom = 16.dp
-                    ),
-                style = TextStyle(
-                    color = Color.White,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Normal
-                )
-            )
-        }
+        )
     }
 }
