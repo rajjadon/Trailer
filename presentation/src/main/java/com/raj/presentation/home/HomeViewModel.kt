22628e87b01f0486c974f70aad7a476ebe4bee8a
Raj Pratap Singh Jadon <rajjadon@gofynd.com>
2023-08-25 18:40:50 +0530

    add favorite ui

--- presentation/src/main/java/com/raj/presentation/home/HomeViewModel.kt
+++ presentation/src/main/java/com/raj/presentation/home/HomeViewModel.kt
@@ -7,6 +7,7 @@ import com.raj.common.extension.invokeOnIO
 import com.raj.common.extension.toSharedFlow
 import com.raj.domain.model.TrendingDomain
 import com.raj.domain.useCase.AddMovieInFavoriteUseCase
+import com.raj.domain.useCase.GetFavoriteMovieListUseCase
 import com.raj.domain.useCase.GetMovieListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -15,7 +16,8 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val getMovieListUseCase: GetMovieListUseCase,
-    private val addMovieInFavoriteUseCase: AddMovieInFavoriteUseCase
+    private val addMovieInFavoriteUseCase: AddMovieInFavoriteUseCase,
+    private val getFavoriteMovieListUseCase: GetFavoriteMovieListUseCase
 ) : ViewModel() {
 
     private val _movieList = MutableSharedFlow<DataState<List<TrendingDomain>>>()
